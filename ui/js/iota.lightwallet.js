55f3f170ec29c1b2d01f4f418f368c3f82a750dd
Atypical <atypical@tuta.io>
2016-12-25 18:37:40 +0100

    Update ccurl interface.

--- ui/js/iota.lightwallet.js
+++ ui/js/iota.lightwallet.js
@@ -1,24 +1,4 @@
-iota.api.attachToTangle = function(trunkTransaction, branchTransaction, minWeightMagnitude, trytes, callback) {
-    // inputValidator: Check if correct hash
-    if (!iota.validate.isHash(trunkTransaction)) {
-        return callback("Invalid trunk transaction");
-    }
-
-    // inputValidator: Check if correct hash
-    if (!iota.validate.isHash(branchTransaction)) {
-        return callback("Invalid branch transaction");
-    }
-
-    // inputValidator: Check if int
-    if (!iota.validate.isInt(minWeightMagnitude)) {
-        return callback("Invalid min weight");
-    }
-
-    // inputValidator: Check if array of trytes
-    if (!iota.validate.isArrayOfTrytes(trytes)) {
-        return callback("Invalid trytes");
-    }
-    
+iota.api.attachToTangle = function(trunkTransaction, branchTransaction, minWeightMagnitude, trytes, callback) {   
     ccurl(trunkTransaction, branchTransaction, minWeightMagnitude, trytes, connection.ccurlPath, function(error, success) {
         if (callback) {
             return callback(error, {"trytes": success})
