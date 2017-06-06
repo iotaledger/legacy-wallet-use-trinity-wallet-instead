be69ff937ff50471516f17bf5499383eba6e34d4
psychlops <jayson@jayson.net>
2017-06-06 10:47:16 -0400

    Added main menu item for Turkish

--- app/js/main.js
+++ app/js/main.js
@@ -571,6 +571,14 @@ var App = (function(App, undefined) {
               },
               type: "checkbox",
               checked: settings.language == "es"
+            },
+            {
+              label: i18n.t("Turkish"),
+              click(item) {
+                App.changeLanguage("tr");
+              },
+              type: "checkbox",
+              checked: settings.language == "tr"
             }
           ]
         }
