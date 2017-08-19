75823eb0e657470fe83173457b9de4975d92fa1b
Paul D Handy <paul.d.handy@gmail.com>
2017-08-19 01:57:18 -0600

    moved local pow to curl.lib.js

--- ui/js/iota.lightwallet.js
+++ ui/js/iota.lightwallet.js
@@ -1,30 +1,12 @@
-var localAttachToTangle = function(trunkTransaction, branchTransaction, minWeightMagnitude, trytes, callback) {
-    console.log("Light Wallet: localAttachToTangle");
-
-    ccurl.ccurlHashing(connection.ccurlProvider, trunkTransaction, branchTransaction, minWeightMagnitude, trytes, function(error, success) {
-        console.log("Light Wallet: ccurl.ccurlHashing finished:");
-        if (error) {
-            console.log(error);
-        } else {
-            console.log(success);
-        }
-        if (callback) {
-            return callback(error, success);
-        } else {
-            return success;
-        }
-    })
-}
-
 var localInterruptAttachingToTangle = function(callback) {
     console.log("Light Wallet: localInterruptAttachingToTangle");
 
-    ccurl.ccurlInterrupt(connection.ccurlProvider);
+    curl.interrupt();
 
     if (callback) {
       return callback();
     }
 }
 
-iota.api.attachToTangle = localAttachToTangle;
+curl.overrideAttachToTangle(iota.api);
 iota.api.interruptAttachingToTangle = localInterruptAttachingToTangle;
